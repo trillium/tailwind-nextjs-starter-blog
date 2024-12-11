18f937e8042eda0f6809f91045fc91297230f823
Trillium Smith <spiteless@gmail.com>
2024-12-11 02:14:32 -0800

    feat: Resolve ESLint warnings for imports in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 // @ts-check
-const { fontFamily } = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
+import { fontFamily } from 'tailwindcss/defaultTheme'
+import colors from 'tailwindcss/colors'
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography'
 
 /** @type {import("tailwindcss/types").Config } */
 module.exports = {
@@ -72,5 +74,5 @@ module.exports = {
       }),
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  plugins: [forms, typography],
 }
